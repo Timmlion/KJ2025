553a141acea4466f0ae12b91f8508923bc09b0f9
Antoni Mazur <sonicmephit@gmail.com>
2025-01-24 23:00:10 +0100

    WaveSpawner update

--- Assets/Scripts/LevelsManager.cs
+++ Assets/Scripts/LevelsManager.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class LevelsManager : MonoBehaviour
 {
-    public GameObject playerBase;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> playerBaseList = new List<GameObject>();
+    public List<GameObject> playerList = new List<GameObject>();
+    public List<GameObject> towerList = new List<GameObject>();
+    public List<GameObject> spawnerList = new List<GameObject>();
+
     void Start()
     {
         
@@ -14,4 +19,6 @@ public class LevelsManager : MonoBehaviour
     {
         
     }
+
+    
 }
--- Assets/Scripts/WavesManager.cs
+++ Assets/Scripts/WavesManager.cs
@@ -11,6 +11,14 @@ public class WavesManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+            if (Input.GetButtonUp("Fire1"))
+            SpawnWave();
+    }
+
+    public void SpawnWave() {
+        foreach (GameObject spawner in GameManager.Instance.LevelsManager.spawnerList)
+        {
+            spawner.GetComponent<Spawner>().spawnBubble();
+        }
     }
 }
