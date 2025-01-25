c74a0e4e8d8a7dec36c7a08a4b01e5e33863da95
Pastew <kpastew@gmail.com>
2025-01-25 02:40:26 +0100

    fix: dotween warning

--- Assets/Pastew/BubbleGfx/BubbleGfxAnimator.cs
+++ Assets/Pastew/BubbleGfx/BubbleGfxAnimator.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BubbleGfxAnimator : MonoBehaviour
 {
@@ -16,16 +17,27 @@ public class BubbleGfxAnimator : MonoBehaviour
     
     [SerializeField] private Ease ease;
 
+    public Tween tweenX;
+    public Tween tweenY;
+    public Tween tweenZ;
+
     void Start()
     {
         RestartAnimation();
     }
 
+    private void OnDestroy()
+    {
+        tweenX.Kill();
+        tweenY.Kill();
+        tweenZ.Kill();
+    }
+
     [ContextMenu(nameof(RestartAnimation))]
     private void RestartAnimation()
     {
-        animationPivot.DOScaleX(maxScaleX * Random.Range(1, maxDurationRandomizer), maxDurationX * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
-        animationPivot.DOScaleY(maxScaleY * Random.Range(1, maxDurationRandomizer), maxDurationY * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
-        animationPivot.DOScaleZ(maxScaleZ * Random.Range(1, maxDurationRandomizer), maxDurationZ * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
+        tweenX = animationPivot.DOScaleX(maxScaleX * Random.Range(1, maxDurationRandomizer), maxDurationX * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
+        tweenY =animationPivot.DOScaleY(maxScaleY * Random.Range(1, maxDurationRandomizer), maxDurationY * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
+        tweenZ =animationPivot.DOScaleZ(maxScaleZ * Random.Range(1, maxDurationRandomizer), maxDurationZ * Random.Range(1, maxScaleRandomizer)).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
     }
 }
