d2006a0a6fc01385789970cd12ea0553c0dc1d3a
Adam Siwek <41082766+Timmlion@users.noreply.github.com>
2025-01-25 15:07:09 +0100

    color change on tower move

--- Assets/Piotrek/Scripts/PlayerController.cs
+++ Assets/Piotrek/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,15 +13,21 @@ public class PlayerController : MonoBehaviour
     public ElementType CurrentElementType = ElementType.Blue;
 
     private TowerController currentTower;
+    private TowerController targetTower;
+    private Vector3 startPosition;
+    private float elapsedTime;
     
     private PlayerInput playerInput;
     [SerializeField] private PlayerGFXController playerGfx;
+    [SerializeField] private float moveDuration = 0.1f;
     private Vector2 moveInput;
     private PlayerState CurrentPlayerState = PlayerState.Idle;
+    [SerializeField] private Light orbLight;
 
     public void InitializePlayer(PlayerInput input)
     {
         playerInput = input;
+        orbLight.color = ElementColor(CurrentElementType);
     }
 
     public void OnLook(InputValue value)
@@ -63,14 +70,38 @@ public class PlayerController : MonoBehaviour
 
     private void SwitchToColor(ElementType elementType)
     {
+        print("Setting color");
+        orbLight.color = ElementColor(elementType);
+        print(orbLight.color);
         CurrentElementType = elementType;
         currentTower.SetTowerColor(CurrentElementType);
         playerGfx.SetColor(CurrentElementType);
     }
-    
+
+    private Color ElementColor(ElementType et)
+    {
+        switch (et)
+        {
+            case ElementType.Yellow:
+                return Color.yellow;
+
+            case ElementType.Red:
+                return Color.red;
+
+            case ElementType.Blue:
+                return Color.blue;
+
+            case ElementType.Green:
+                return Color.green;
+
+            default:
+                Debug.LogWarning("Unknown ElementType, returning default white color.");
+                return Color.white;
+        }
+    }
     public void OnPreviousTower(InputValue value)
     {
-        if (CurrentPlayerState == PlayerState.Shooting)
+        if (CurrentPlayerState != PlayerState.Idle)
         {
             return;
         }
@@ -89,7 +120,7 @@ public class PlayerController : MonoBehaviour
 
     public void OnNextTower(InputValue value)
     {
-        if (CurrentPlayerState == PlayerState.Shooting)
+        if (CurrentPlayerState != PlayerState.Idle)
         {
             return;
         }
@@ -113,16 +144,18 @@ public class PlayerController : MonoBehaviour
             // TODO: Consider how to handle this. Is player flying to next tower?
         }
 
-        if (value.Get<float>() > 0.6f)
+
+        if (value.Get<float>() > 0.6f && CurrentPlayerState == PlayerState.Idle)
         {
             currentTower.CreateBullet(CurrentElementType, AttackType.Basic);
             CurrentPlayerState = PlayerState.Shooting;
         }
-        else if (value.Get<float>() < 0.3f)
+        else if (value.Get<float>() < 0.3f && CurrentPlayerState == PlayerState.Shooting)
         {
             currentTower.LaunchBullet();
             CurrentPlayerState = PlayerState.Idle;
         }
+        
     }
     
     private void OnSpecialAttack(InputValue value)
@@ -130,19 +163,34 @@ public class PlayerController : MonoBehaviour
         OnBasicAttack(value); // TODO: this
     }
 
-    private void Update()
-    {
-        // Debug only
-        //Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * (Time.deltaTime * 5f);
-        //transform.Translate(movement);
-    }
-
     private void MoveToTower(TowerController tower)
     {
-        //TODO: Implement flowing to tower
+        CurrentPlayerState = PlayerState.MovingToTower;
+        targetTower = tower;
+        startPosition = transform.position;
+        elapsedTime = 0f;
+        
         transform.position = tower.transform.position;
     }
     
+    private void Update()
+    {
+        if (CurrentPlayerState == PlayerState.MovingToTower)
+        {
+            // Gradually move towards the target
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp01(elapsedTime / moveDuration);
+            transform.position = Vector3.Lerp(startPosition, targetTower.transform.position, t);
+
+            // Stop moving when we reach the target
+            if (t >= 1.0f)
+            {
+                CurrentPlayerState = PlayerState.Idle;
+            }
+        }
+    }
+
     public void SetCurrentTower(TowerController tower)
     {
         currentTower = tower;
