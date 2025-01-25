6d4172469c65fb5fc4f9422642b36c9cf7d287f5
Adam Siwek <41082766+Timmlion@users.noreply.github.com>
2025-01-25 02:29:00 +0100

    audio player update

--- Assets/Adam/AudioPlayer.cs
+++ Assets/Adam/AudioPlayer.cs
@@ -3,25 +3,13 @@ using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
-    public List<AudioClip> PopAudioClips; // List of audio clips for enemy death sounds
     private AudioSource audioSource;
 
     // Method to play a random EnemyDeath sound
     public void PlayEnemyDeathSound()
     {
-        if (PopAudioClips == null || PopAudioClips.Count == 0)
-        {
-            Debug.LogError("PopAudioClips list is empty or not assigned.");
-            return;
-        }
-
-        // Get a random clip from the list
-        AudioClip randomClip = PopAudioClips[Random.Range(0, PopAudioClips.Count)];
-
-        // Play the random clip
-        audioSource.PlayOneShot(randomClip);
-
-        Debug.Log($"Playing clip: {randomClip.name}");
+        
+        // Implement player death sounds
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,10 +21,6 @@ public class AudioPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            // Play a random EnemyDeath sound
-            PlayEnemyDeathSound();
-        }
+
     }
 }
