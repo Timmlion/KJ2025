ddad74873a1f0b1a0472aa4a0f06b24840213d55
Pastew <kpastew@gmail.com>
2025-01-26 11:56:26 +0100

    hp bar anim feedback

--- Assets/HpBar/HPBar.cs
+++ Assets/HpBar/HPBar.cs
@@ -1,25 +1,35 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
-    public Image healthBarImage; 
-    public GameObject gfxAnchor; 
+    public Image healthBarImage;
+    public GameObject gfxAnchor;
+    [SerializeField] private float _shakeDuration = 0.3f;
+    [SerializeField] private float _shakeStrength = 30f;
 
     void Start()
     {
-        UpdateHealthBar(1); 
+        UpdateHealthBar(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+            UpdateHealthBar(0.4f);
     }
 
     public void UpdateHealthBar(float value)
     {
-        // gfxAnchor.gameObject.SetActive(value < 0.95f);
+        gfxAnchor.gameObject.SetActive(value < 0.95f);
         gfxAnchor.gameObject.SetActive(true);
-        
+
         if (healthBarImage != null)
         {
             healthBarImage.fillAmount = value;
         }
-    }
 
+        gfxAnchor.transform.DOShakePosition(_shakeDuration, _shakeStrength);
+    }
 }
