029fb4cf13b32e42387cd2a788d288b4754efd21
Antoni Mazur <sonicmephit@gmail.com>
2025-01-26 11:34:14 +0100

    Additional keyboard imput for restart and start

--- Assets/Scripts/MenuGUI.cs
+++ Assets/Scripts/MenuGUI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class MenuGUI : MonoBehaviour
 {
     public GameObject startScreen;
     public GameObject gameScreen;
     public GameObject endgameScreen;
+    public TMP_Text waveText;
 
     void Start()
     {
@@ -21,14 +23,14 @@ public class MenuGUI : MonoBehaviour
                 if (gamepad == null)
                     return; // No gamepad connected.
 
-                if (gamepad.rightTrigger.wasPressedThisFrame)
+                if (gamepad.rightTrigger.wasPressedThisFrame || Input.GetKeyDown(KeyCode.Space))
                 {
                     StartGame();
                 }
 
             }
 
-        if (endgameScreen.activeSelf)
+        if (endgameScreen.activeSelf | Input.GetKeyDown(KeyCode.Space))
             { 
                 var gamepad = Gamepad.current;
                 if (gamepad == null)
@@ -40,7 +42,14 @@ public class MenuGUI : MonoBehaviour
                 }
 
             }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RestartGame();
+            }
     }
+
+
 
     public void StartGame() {
         // change to async <-
